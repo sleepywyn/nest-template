d4d0bc94cc9aa95f86b51486894ad6d817c42029
sleepywyn <sleepywyn@gmail.com>
2022-03-15 12:15:13 +0800

    initial commit includes: grpc microservice template

---
+++ src/grpc-client.options.ts
@@ -0,0 +1,11 @@
+import { ClientOptions, Transport } from '@nestjs/microservices';
+import { join } from 'path';
+
+export const grpcClientOptions: ClientOptions = {
+  transport: Transport.GRPC,
+  options: {
+    url: 'localhost:5005',
+    package: 'hero', // ['hero', 'hero2']
+    protoPath: join(__dirname, './hero/hero.proto'), // ['./hero/hero.proto', './hero/hero2.proto']
+  },
+};
