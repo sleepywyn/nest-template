da5b11095d051899cf71d4c5e48a294d92171412
sleepywyn <sleepywyn@gmail.com>
2022-03-16 15:15:40 +0800

    1. move env files into src folder & nest-cli.json tweak. Now nest-cli will move env files into dist in production mode 2. add custom configurations. The recommended config module will merge bash environment variables, env files and custom configs into a a big map. 3. add cross-env pkg to solve the windows startup issue.

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,6 +1,9 @@
 import { Module } from '@nestjs/common';
 import { ConfigModule } from '@nestjs/config';
 import { validate, Environment } from './env.validation';
+import { join } from 'path';
+
+import configuration from './config/configuration';
 
 import { HeroModule } from './hero/hero.module';
 import { AuthModule } from './auth/auth.module';
@@ -11,9 +14,10 @@ import { AuthModule } from './auth/auth.module';
     AuthModule,
     ConfigModule.forRoot({
       validate,
-      envFilePath: process.env.NODE_ENV === Environment.Development ? '.dev.env' 
-                :  process.env.NODE_ENV === Environment.Test ? '.test.env'
-                :  process.env.NODE_ENV === Environment.Production ? '.prod.env' : '.dev.env', // add to nest-cli.json  "assets": [{"include": "*.env", "outDir": "./dist"}],
+      envFilePath: process.env.NODE_ENV === Environment.Development ? join(__dirname, '.env.dev') // the default folder is not dist if not using path join
+                :  process.env.NODE_ENV === Environment.Test ? join(__dirname, '.env.test')
+                :  process.env.NODE_ENV === Environment.Production ? join(__dirname, '.env.prod') : join(__dirname, '.dev.env'), // add to nest-cli.json  "assets": [{"include": "*.env", "outDir": "./dist"}],
+      load: [configuration],
       isGlobal: true
     })
   ]
--- src/auth/auth.service.ts
+++ src/auth/auth.service.ts
@@ -15,6 +15,9 @@ export class AuthService {
     console.log(`NODE_ENV: ${process.env.NODE_ENV}`)
     console.log(`received token: ${token}`);
     console.log(`config token: ${this.configService.get<String>('bearer')}`); // 注意配置大小写
+    console.log(`access from process.env: ${process.env.bearer}`);
+    console.log(`custom config: ${this.configService.get<string>('database.host')}`)
+    // console.log(JSON.stringify(process.env));
     if (token === this.configService.get<String>('bearer')) return {issuer: 'auth-service'};
     return null;
   }
